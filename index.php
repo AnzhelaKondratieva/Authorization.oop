d3bbdc361f5b30309c07484496917682d47e5b53
AnzhelaKondratieva <kondratieva.anzhelika92@gmail.com>
2019-04-25 10:40:13 +0300

    form

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<form action="" method="POST">
+ <label for="login">Логин</label><br>
+ <input name="text" type="text" id="login"><br>
+ <label for ="password">Пароль</label><br>
+ <input type="password" name="password" id="password"><br>
+ <input type="submit" value="Войти">
+</form>
+</body>
+</html>
