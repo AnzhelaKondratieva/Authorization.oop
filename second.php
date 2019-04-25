e77dbd43ab1617cb764365182e856856910cdf2f
AnzhelaKondratieva <kondratieva.anzhelika92@gmail.com>
2019-04-25 13:30:12 +0300

    languages

--- second.php
+++ second.php
@@ -2,15 +2,37 @@
 session_start();
 ?>
 <?php
-class Languages
-{
-	public $lang;
+class Trans {
+	public $lang = ['ru' => 'Привет', 'en' => 'Hi', 'ua' => 'Привіт', 'it' => 'Salvo'];
+
+	public function _construct($lang){
+		$this->lang = $lang;
+	}
+	public function greeting() {
+		if (isset($_SESSION['user']) && isset($_SESSION['lang'])) {
+			echo $this->lang[$_SESSION['lang']]. ', '. $_SESSION['user'];
+	}
+		elseif (isset($_SESSION['user']) && !isset($_SESSION['lang'])) {
+	?>
+	<html>
+<p> Выберите необходимый язык</p>
+<form method="POST">
+<select name="languages">
+<option selected value="en" name="lang">Английский</option>
+<option value="ru" name="lang">Русский</option>
+<option value="it" name="lang">Итальянский</option>
+<option value="ua" name="lang">Украинский</option>
+</select>
+<input type="submit" name="click" value="Отправить">
+</form>
+<?php
+if(isset($_POST['click'])) {
+	$_SESSION['lang'] = $_POST['languages'];
+	echo $this->lang[$_SESSION['lang']]. ', '. $_SESSION['user'];
 }
-$english = new Languages();
-$english->lang = 'Hi';
-$russian = new Languages();
-$russian->lang = 'Привет';
-$ukrainian = new Languages();
-$ukrainian->lang = "Привіт";
-$italian = new Languages();
-$italian->lang = 'Salvo';
+}
+	}
+}
+$obj = new Trans();
+$obj->greeting();
+?>
