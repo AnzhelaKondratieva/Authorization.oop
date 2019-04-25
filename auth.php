6ba81747f1ddecd1a63fc098af6874692f081406
AnzhelaKondratieva <kondratieva.anzhelika92@gmail.com>
2019-04-25 10:41:40 +0300

    users added

--- auth.php
+++ auth.php
@@ -10,46 +10,50 @@ class Users
 	public function _construct($login, $password, $lang) {
 		$this->login - $login;
 		$this->password - $password;
+		$this->lang - $lang;
 	}
-	function present() {
-		echo 'Человека зовут'. $this->login;
+	function authorization() {
+		if (isset($_POST['text']) && $_POST['text'] == $this->login 
+ 			&& $_POST['password'] == $this->password){
+				$_SESSION['user'] = $this->login;
+				$_SESSION['lang'] = $this->lang;
+				header("Location: second.php");
+			}
 	}
+}
+class Languages
+{
+	public $lang;
 }
 
 $user1 = new Users();
 $user1->login = 'Vasisualiy';
+$user1->password = '12345';
+$user1->lang = 'ru';
 $user1->present();
+$user2 = new Users();
+$user2->login = 'Afanasiy';
+$user2->password = '54321';
+$user2->lang = 'en';
+$user2->present();
+$user3 = new Users();
+$user3->login = 'Petro';
+$user3->password = 'EkUC42nzmu';
+$user3->lang = 'ua';
+$user3->present();
+$user4 = new Users();
+$user4->login = 'Pedrolus';
+$user4->password = 'Cogito_ergo_sum';
+$user4->lang = 'it';
+$user4->present();
+$user4 = new Users();
+$user4->login = 'Sasha';
+$user4->password = 'Alea_est_jacta';
+$user4->present();
 
 
 
 
-//  $users = [
-// [
-// 'login' => 'Vasisualiy', 'password' => '12345', 'lang' => 'ru'
-// ],
-// [
-// 'login' => 'Afanasiy', 'password' => '54321', 'lang' => 'en'
-// ],
-// [
-// 'login' => 'Petro', 'password' => 'EkUC42nzmu', 'lang' => 'ua'
-// ],
-// [
-// 'login' => 'Pedrolus', 'password' => 'Cogito_ergo_sum', 'lang' => 'it'
-// ],
-// [
-// 'login' => 'Sasha', 'password' => 'Alea_est_jacta',
-// ],
-// ];
-
-//  foreach ($users as $user) {
-//  		if (isset($_POST['text']) && $_POST['text'] == $user['login'] 
-//  			&& $_POST['password'] == $user['password']){
-// 				$_SESSION['user'] = $user['login'];
-// 				$_SESSION['lang'] = $user['lang'];
-// 				header("Location: second.php");
-// 			}
-// 		}
-// 	?>
 <html><body>
 	Неверный пароль!
 <form method="POST">
