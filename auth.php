bbca48fb291ebc83285a23508f78825b85622c3a
AnzhelaKondratieva <kondratieva.anzhelika92@gmail.com>
2019-08-17 20:34:25 +0300

    first commit

--- auth.php
+++ auth.php
@@ -1,77 +1,85 @@
 <?php
 session_start();
 ?>
-<?php
-$users = [
-[
-'login' => 'Vasisualiy', 'password' => '12345', 'lang' => 'ru'
-],
-[
-'login' => 'Afanasiy', 'password' => '54321', 'lang' => 'en'
-],
-[
-'login' => 'Petro', 'password' => 'EkUC42nzmu', 'lang' => 'ua'
-],
-[
-'login' => 'Pedrolus', 'password' => 'Cogito_ergo_sum', 'lang' => 'it'
-],
-[
-'login' => 'Sasha', 'password' => 'Alea_est_jacta',
-],
-];
+<?php 
+
+
+// класс USERS!
 class Users
 {
-	public $login;
-	public $password;
-	public $lang;
-	public function _construct($login, $password, $lang) {
-		$this->login = $login;
-		$this->password = $password;
-		$this->lang = $lang;
-	}
-	public function getUser($login, $password, $lang) {
-			if (isset($_SESSION['user'])) {
-				$number = $_SESSION['user'];
-				return new Users($users[$number]['login'], $users[$number]['password'], $users[$number]['lang']);
-			}
-			if ($_POST['text'] && $_POST['password']){
+public $login;
+public $password;
+public $lang;
+public function __construct($login, $password, $lang)
+{
+$this->login = $login;
+$this->password = $password;
+$this->lang = $lang;
+}
+public function present()
+{
+return "my name is {$this->login} ваш язык {$this->lang}";
+}
+public $users = [
+['login' => 'Vasisualiy', 'password' => '12345', 'lang' => 'ru'],
+['login' => 'Afanasiy', 'password' => '54321', 'lang' => 'en'],
+['login' => 'Petro', 'password' => 'EkUC42nzmu', 'lang' => 'ua'],
+['login' => 'Pedrolus', 'password' => 'Cogito_ergo_sum', 'lang' => 'it'],
+['login' => 'Sasha', 'password' => 'Alea_est_jacta', ],
+];
+// ФУНКЦИЯ Создание юзера!
+function getUser()
+{
+//если юзер авторизован
+if (isset($_SESSION['user'])) {
+$number = $_SESSION['user'];
+return new Users($users[$number]['login'], $users[$number]['password'], $users[$number]['lang']);
+}
+//если юзер не авторизован
+if ($_POST['login'] && $_POST['password']){
 foreach ($users as $key => $value) {
-if ($_POST['text'] == $value['login'] && $_POST['password'] == $value['password']) {
+if ($_POST['login'] == $value['login'] && $_POST['password'] == $value['password']) {
 $_SESSION['user'] = $key;
 return new Users($_POST['login'], $_POST['password'], $value['lang']);
 }
 }
+}
 return null;
 }
+
 }
-	public function authorization() {
-		$this->login = $login;
-		$this->password = $password;
-		$this->lang = $lang;
-		if (isset($_POST['text']) && $_POST['text'] == $this->login 
- 			&& $_POST['password'] == $this->password){
-				$_SESSION['user'] = $this->login;
-				$_SESSION['lang'] = $this->lang;
-				header("Location: second.php");
-			}
-			
-			}
-	}
+class Trans
+{
+public $lang;
+public $user;
 
-$user2 = new Users();
-$user2->authorization();
-$user2->getUser('Vasisualiy', '12345', 'ru');
-?>
+public function __construct()
+{
+$this->user = getUser('Vasisualiy');
+$this->lang = 'ua'; // язык по умолчанию, отбражаеться если не выбрано другое
 
-
-<html><body>
-	Неверный пароль!
-<form method="POST">
-<input type="submit" value="Вернуться" name="back">
-</form>
-<?php
-if (isset($_POST['back']) ) {
-	header("Location: index.php");
+//тащим язык из настроек пользователя
+if (!empty($this->user->lang)) {
+$this->lang = $this->user->lang;
 }
-?>
-</body></html>
+// если все таки в сесии лежит выбранный язык.
+if (isset($_SESSION['lang'])) {
+$this->lang = $_SESSION['lang'];
+}
+}
+
+//переводимых фраз много, надо их как-то идентифицировать
+public function getLang($phrase)
+{
+return $trans[$phrase][$this->lang];
+}
+}
+
+if(isset($_POST['lang'])){
+$_SESSION['lang'] = $_POST['lang'];
+}
+
+$trans = new Trans();
+echo $trans->getLang('hello');
+$user = getUser('Vasisualiy')->present();
+echo $trans->getLang('bye');
